505b9f08baadfe8f021136dc42639cd8fb2e0dfe
Leo <leonardmartirosyan95@gmail.com>
2024-05-06 19:54:30 +0400

    add throttle middleware

--- routes/api.php
+++ routes/api.php
@@ -14,21 +14,25 @@ use Illuminate\Support\Facades\Route;
 
 
 
-//api/admin
-Route::middleware(['auth:api','verified','admin',])->prefix('admin')->group(function () {
-    Route::apiResource('users', UserController::class);
+//api/auth
+Route::middleware('throttle:10,1')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/forgot-password', [ForgotPasswordController::class, 'forgotPassword']);
+    Route::post('/check-code', [ForgotPasswordController::class, 'checkResetCode']);
+    Route::post('/reset-password', [ForgotPasswordController::class, 'resetPassword']);
 });
 
-//api/auth
 Route::middleware(['auth:api', 'verified'])->prefix('auth')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/refresh', [AuthController::class, 'refresh']);
     Route::get('/user', [AuthController::class, 'user']);
 });
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/forgot-password', [ForgotPasswordController::class, 'forgotPassword']);
-Route::post('/check-code', [ForgotPasswordController::class, 'checkResetCode']);
-Route::post('/reset-password', [ForgotPasswordController::class, 'resetPassword']);
+
+//api/admin
+Route::middleware(['auth:api','verified','admin',])->prefix('admin')->group(function () {
+    Route::apiResource('users', UserController::class);
+});
+
 
 //api/department
 Route::middleware(['auth:api','verified','department'])->prefix('department')->group(function () {
