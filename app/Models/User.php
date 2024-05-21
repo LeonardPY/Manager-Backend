e11ff9ef1eed904107fb43ee19ce74b545b6b803
Hrayr Ashchyan <hrayrashchyan94@gmail.com>
2024-05-21 21:19:41 +0400

    add user upload logo also fix category return  message

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,8 @@ use App\Models\Traits\Filterable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Queue\Connectors\BeanstalkdConnector;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 
